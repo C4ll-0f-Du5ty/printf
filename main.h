958b55c81fdff6f2cdda1ccc3344f78a90a7c5eb
Z-ajamy <abdorahman0283@gmail.com>
2023-10-18 02:34:59 +0300

    updated

--- main.h
+++ main.h
@@ -1,47 +1,43 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <limits.h>
-#include <stdarg.h>
 #include <inttypes.h>
 
 /**
- * struct note - Struct for handling format specifiers
+ * struct FormatHandler - Struct for handling format specifiers
  * @specifier: The format specifier
  * @handler: Pointer to the corresponding handler function
  *
  */
 
-typedef struct note
+typedef struct FormatHandler
 {
 	char specifier;
-
-	int (handler)(va_list, int);
-
+	int (*handler)(va_list args, int *counter);
 } FormatHandler;
 
 int _printf(const char *format, ...);
 int _putchar(char c);
-int print_char(va_list args, int *counter);
 int _puts(char *str);
-int print_string(va_list args, int *counter);
-int _printNumbers(int number);
-int print_int(va_list args, int *counter);
-int print_percent(va_list args, int *counter);
-void decToBinary(int n);
-int print_binary(va_list args, int *counter);
 int _specifiers(const char *format, va_list args);
+
+int print_char(va_list args, int *counter);
+int print_string(va_list args, int *counter);
+int print_percent(va_list args, int *counter);
+int print_int(va_list args, int *counter);
 int print_unsigned(va_list args, int *counter);
+int print_binary(va_list args, int *counter);
 int print_hex(va_list args, int *counter);
 int print_HEX(va_list args, int *counter);
 int print_octal(va_list args, int *counter);
 int print_pointer(va_list args, int *counter);
 int print_reverse(va_list args, int *counter);
 int print_only_string(va_list args, int *counter);
-
 
 #endif
