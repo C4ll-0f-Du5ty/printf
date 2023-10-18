7a7ad83e30e8d6649f4a2c8fb548c33adf4cb37f
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2023-10-17 17:34:17 -0700

    updated

--- _printf.c
+++ _printf.c
@@ -25,31 +25,3 @@ int _printf(const char *format, ...)
 	va_end(args);
 	return (result);
 }
-
-/**
- * print_reverse - Prints a string in reverse.
- * @args: A list of arguments containing the string to be printed.
- * @counter: A pointer to the counter for tracking
- * the number of characters printed.
- *
- * Return: The total number of characters printed.
- */
-
-int print_reverse(va_list args, int *counter)
-{
-	char *s = va_arg(args, char*);
-	int i;
-
-	if (s)
-	{
-		int len = strlen(s);
-
-		for (i = len - 1; i >= 0; i--)
-		{
-			_putchar(s[i]);
-			(*counter)++;
-		}
-		return (*counter);
-	}
-	return (*counter);
-}
--- _specifier.c
+++ _specifier.c
@@ -11,16 +11,14 @@ int _specifiers(const char *format, va_list args)
 {
 	int counter = 0;
 	int i;
+	int found = 0;
 
 				FormatHandler formatHandlers[] = {
-				{'c', print_char},
-				{'s', print_string}, {'%', print_percent},
-				{'d', print_int}, {'i', print_int},
-				{'u', print_unsigned},
+				{'c', print_char}, {'s', print_string}, {'%', print_percent},
+				{'d', print_int}, {'i', print_int}, {'u', print_unsigned},
 				{'b', print_binary}, {'x', print_hex},
 				{'X', print_HEX}, {'o', print_octal},
-				{'p', print_pointer},
-				{'r', print_reverse}, {'S', print_only_string},
+				{'p', print_pointer}, {'S', print_only_string},
 				{'\0', NULL},
 				};
 	while (*format)
@@ -31,15 +29,18 @@ int _specifiers(const char *format, va_list args)
 			for (i = 0; formatHandlers[i].specifier != '\0'; i++)
 			{
 				if (formatHandlers[i].specifier == *format)
-				{
-					counter += formatHandlers[i].handler(args, &counter);
+				{counter += formatHandlers[i].handler(args, &counter);
+					found = 1;
 					break;
 				}
 			}
+			if (found == 0)
+			{_putchar(*format);
+				counter++;
+			}
 		}
 		else
-		{
-			_putchar(*format);
+		{_putchar(*format);
 			counter++;
 		}
 		format++;
